9ecac8502f4be0ad88c27b4a090ab9d76b90583d
sahilstan <me@sahilsharma.co>
2018-05-01 01:02:55 +0400

    Updated POW

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ class Block {
     }
 
     mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) != Array(difficulty + 1).join("0")){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
             this.nonce++;
             this.hash = this.calculateHash();
         }
